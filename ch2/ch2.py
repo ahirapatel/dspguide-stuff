34aefd7843378ad357634af341ea1b9d82660f55
Hiral Patel <oldsaridin666@gmail.com>
2017-08-29 16:19:40 -0400

    More of ch2

--- ch2/ch2.py
+++ ch2/ch2.py
@@ -17,6 +17,7 @@ def standard_deviation(data):
     std = variance ** .5
     return std
 
+#print(standard_deviation([996, 868, 855, 956, 867, 933, 866, 887, 936, 901, 818, 956]))
 
 def calc_running_stats(data):
     # Three needed params for running stats
@@ -61,10 +62,29 @@ def calc_running_stats_modded(data):
         std = std ** .5
 
 
-# I think we are supposed to assume pre-binned data, atleast for 1c on non-computer?
-# Let's assume data is a dict here, where mapping is k,v => sample_value, sample_occurrences
-# EDIT: OH FROM TABLE-2.3 ... which is a 404
-def histo_mean(data):
-    #mean = 0
-    #for sample_value, sample_occurrences in data.items():
-    #    mean += 
+# for 1.c
+def histo_stats(data):
+    # 10 bits == 1024
+    hist = [None] * 1024 # Should be 1024 in size, for 0 to 1023 as values
+
+    # Init to 0
+    for i in range(len(hist)):
+        hist[i] = 0
+
+    for val in data:
+        hist[val] += 1
+
+    mean = 0
+    for i in range(len(hist)):
+        mean += i * hist[i]   # i == value, hist[i] == num occurrences of value
+
+    mean /= len(data)
+
+    variance = 0
+    for i in range(len(hist)):
+        variance += hist[i] * (i - mean)**2
+    variance /= len(data) - 1
+    std = variance ** .5
+
+
+
