07161e46e29461c92558f5048b6d36a81cd5ef86
hap <oldsaridin666@gmail.com>
2017-10-28 16:38:06 -0400

    ch12: homework and homework-comp

--- ch12/homework-comp.py
+++ ch12/homework-comp.py
@@ -7,6 +7,7 @@ CHAPTER 12: THE FAST FOURIER TRANSFORM
 """
 1. Write (or copy) an FFT and an IFFT subroutine. 
 """
+import numpy.fft
 
 """
 2. Test that the subroutines are operating correctly:
@@ -23,7 +24,30 @@ real & imaginary parts.  (To make the frequency spectra easier to graph, you
 will probably want to use random numbers that have a mean of zero).   Does
 the frequency domain have the proper symmetry? Explain 
 """
+from random import random
+import matplotlib.pyplot
+from numpy import absolute, angle, real, imag
 
+# a.
+random_signal = [random() + (random()*(0+1j)) for x in range(256)]
+fft_out = numpy.fft.fft(random_signal)
+reconstructed_signal = numpy.fft.ifft(fft_out)
+delta = [abs(a-b) for a,b in zip(random_signal, reconstructed_signal)]
+# The max difference is at the range of double precision errors
+print(max(delta))
+matplotlib.pyplot.plot(delta)
+matplotlib.pyplot.show()
+
+# b.
+random_signal = [random()*1j for x in range(256)]
+fft_out = numpy.fft.fft(random_signal)
+mag, phase = absolute(fft_out), angle(fft_out)
+mag_real = real(mag)
+mag_imag = imag(mag)
+# Looks pretty symmetric barring the DC offset at 0
+matplotlib.pyplot.plot(mag_real)
+matplotlib.pyplot.plot(mag_imag)
+matplotlib.pyplot.show()
 
 """
 3. Use the FFT in a spectral analysis problem:
@@ -39,7 +63,29 @@ of the 1,024,000 point signal (break the signal into 1024 sample segments,
 calculate the FFTs, and average the magnitudes). Is the sine wave visible in
 the averaged spectrum?
 """
+from random import gauss
+from math import sin, pi
+# a.
+sig = [gauss(0, 1) + (.1 * sin(.2*2*pi*x)) for x in range(1024000)]
 
+# b. sine wave is not visible
+matplotlib.pyplot.plot(sig[:1024])
+matplotlib.pyplot.show()
+
+# c. sine wave is not visible
+fft_out = numpy.fft.fft(sig[:1024])
+mag, phase = absolute(fft_out), angle(fft_out)
+matplotlib.pyplot.plot(real(mag))
+matplotlib.pyplot.show()
+
+# d. sine wave is WAAAAAAY more visible
+diced_data = [sig[i:i+1024] for i in range(0, len(sig), 1024)]
+diced_fft = [absolute(numpy.fft.fft(data)) for data in diced_data] # gets polar magnitude
+# my god, use zip() with 1000 input values
+summed_fft = [sum(lots_of_vals) for lots_of_vals in zip(*diced_fft)]
+averaged_fft = [x / 1024 for x in summed_fft]
+matplotlib.pyplot.plot(real(averaged_fft))
+matplotlib.pyplot.show()
 
 """
 4.  Answer the following questions.
@@ -51,4 +97,8 @@ domain signals have a mean of zero?
 c. In step 2b, if the random signal were placed in the imaginary part, and
 all zeros placed in the real part, how would the frequency domain be changed?
 """
-
+# a. Not much, FFT should be more accurate due to less floating point operations.
+#    However, I can find many papers online about FFT inaccuracies when poorly
+#    implemented.
+# b. The noise averages out?
+# c. Yes it would be changed.
--- ch12/homework.py
+++ ch12/homework.py
@@ -11,7 +11,19 @@ frequency spectrum of the corresponding complex DFT.
 samples 0 to 8 of the real part:        1, 2,-1,-2, 0, 1, 2, 3, 2 
 samples 0 to 8 of the imaginary part:   0, 2, 4, 5,-3,-2, 1, 1, 0
 """
-
+import numpy.fft
+real = [1, 2,-1,-2, 0, 1, 2, 3, 2]
+imag = [0, 2, 4, 5,-3,-2, 1, 1, 0]
+data = [complex(r,i) for r,i in zip(real, imag)]
+orig_sig = numpy.fft.irfft(data)
+print(data)
+print(orig_sig)
+complex_fft = numpy.fft.fft(orig_sig)
+print(complex_fft)
+# Should just be complex conjugates, confirmed with above code.
+# NOTE: Sample 0 is dc offset so it is not mirrored
+# samples 9 to 16 of the real part:         3,  2, 1, 0, -2, -1,  2, 1
+# samples 9 to 16 of the imaginary part:   -1, -1, 2, 3, -5, -4, -2, 0
 
 """
 2. A time domain signal, consisting of a real part, rex[n], and an imaginary
@@ -25,7 +37,35 @@ odd parts.
 b. What would be the spectrum if the values in imx[n] were set to zero?
 e. What would be the spectrum if the values in rex[n] were set to zero?
 """
-     
+from numpy import absolute
+import matplotlib.pyplot
+real = [1, 2,-1,-2, 1, 0, 2, 3]
+imag = [3, 2, 4, 5,-1,-2, 1, 1]
+# a. Why? I am confused about a. Do I do even/odd decomposition or just
+#    every other sample? What does this even show?
+def even_odd_decomposition(x):
+    even = []
+    odd = []
+    N = len(x) - 1
+    for n in range(len(x)):
+        even.append((x[n] + x[N-n])/2)
+        odd.append((x[n] - x[N-n])/2)
+    odd[0] = 0 # Definition explicitly says odd[0] = 0
+    even[0] = x[0] # Definition explicitly says even[0] = x[0]
+    return even, odd
+
+data = [complex(r,i) for r,i in zip(real, imag)]
+matplotlib.pyplot.plot(absolute(numpy.fft.fft(data)))
+matplotlib.pyplot.show()
+# b.
+data = [complex(r,0) for r in real]
+matplotlib.pyplot.plot(absolute(numpy.fft.fft(data)))
+matplotlib.pyplot.show()
+# c.
+data = [complex(0,i) for i in imag]
+matplotlib.pyplot.plot(absolute(numpy.fft.fft(data)))
+matplotlib.pyplot.show()
+
 
 """
 3.  Suppose you want to conduct a spectral analysis of a signal containing
@@ -38,3 +78,21 @@ lengths of: 256, 1024, and 4096. (Ignore the calculation time for averaging
 the frequency spectra).
 b. Repeat (a) using the FFT algorithm.  
 """
+
+# a. 1003520 / 64 = 15680
+#    (1 * 64^2) * 15680 = 64225280
+#    1003520 / 256 = 3920
+#    (1 * 256^2) * 3920 = 256901120
+#    1003520 / 1024 = 980
+#    (1 * 1024^2) * 980 = 1027604480
+#    1003520 / 4096 = 245
+#    (1 * 4096^2) * 245 = 4110417920 micros
+
+# b. 1003520 / 64 = 15680
+#    (1.5 * 64*log64) * 15680 = 9031680
+#    1003520 / 256 = 3920
+#    (1.5 * 256*log256) * 3920 = 12042240
+#    1003520 / 1024 = 980
+#    (1.5 * 1024*log1024) * 980 = 15052800
+#    1003520 / 4096 = 245
+#    (1.5 * 4096*log4096) * 245 = 18063360
